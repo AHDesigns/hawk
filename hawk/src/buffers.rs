ab0219b7c71755ba13eca552a2652b8cecc73c8e
Adam Hall <adamhalldesigns@gmail.com>
2021-12-23 18:15:29 +0000

    start playing around with buffer+display link

--- hawk/src/buffers.rs
+++ hawk/src/buffers.rs
@@ -1,3 +1,4 @@
+use crate::util::Pos;
 use std::{fs::read_to_string, path::Path};
 
 pub struct Buffer {
@@ -37,6 +38,32 @@ impl Buffer {
   pub fn line_break(&mut self) {
     self.text.push("".to_string())
   }
+
+  pub fn find_char_positions(&self, c: char) -> Vec<Pos> {
+    // this would also work for string patterns
+
+    self
+      .text
+      .iter()
+      .enumerate()
+      .filter_map(|(row, l)| {
+        let matches: Vec<Pos> = l
+          .match_indices(c)
+          .map(|(column, _)| Pos {
+            row: row as u8,
+            column: column as u8,
+          })
+          .collect();
+
+        if matches.is_empty() {
+          None
+        } else {
+          Some(matches)
+        }
+      })
+      .flatten()
+      .collect()
+  }
 }
 
 pub fn open_buffer(path: &Path) -> Result<Buffer, String> {
@@ -47,5 +74,37 @@ pub fn open_buffer(path: &Path) -> Result<Buffer, String> {
       name: path.file_name().unwrap().to_owned().into_string().unwrap(),
       text: buf.lines().map(|l| l.to_string()).collect(),
     }),
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::{buffers::Buffer, util::Pos};
+
+  #[test]
+  fn test_find_char_positions() {
+    let mut buffer = Buffer::new(String::from("name"));
+    buffer.append_text("here is some text".to_string());
+
+    assert_eq!(
+      buffer.find_char_positions('s'),
+      vec![Pos::new(0, 6), Pos::new(0, 8)]
+    );
+
+    buffer.line_break();
+    buffer.append_text("sammy snake sss".to_string());
+
+    assert_eq!(
+      buffer.find_char_positions('s'),
+      vec![
+        Pos::new(0, 6),
+        Pos::new(0, 8),
+        Pos::new(1, 0),
+        Pos::new(1, 6),
+        Pos::new(1, 12),
+        Pos::new(1, 13),
+        Pos::new(1, 14)
+      ]
+    );
   }
 }
